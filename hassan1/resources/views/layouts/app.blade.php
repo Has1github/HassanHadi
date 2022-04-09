30cd5e030f2f43aff7b2f587270f74049ab98469
Has1github <hassanhadimowla3@gmail.com>
2022-04-10 01:33:45 +0300

    تنصيب jetstreem

--- hassan1/resources/views/layouts/app.blade.php
+++ hassan1/resources/views/layouts/app.blade.php
@@ -1,33 +1,46 @@
-{{-- <html>
-<head>
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    @livewireStyles
-</head>
-<body>
-    {{$slot}}
- 
+        <title>{{ config('app.name', 'Laravel') }}</title>
 
- 
-    @livewireScripts
-</body>
-</html> --}}
-<!doctype html>
-<html>
-<head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-  <link rel="stylesheet" href="https://unpkg.com/flowbite@1.4.1/dist/flowbite.min.css" />
-  @livewireStyles
-</head>
-<body>
-    {{$slot}}
-  <h1 class="text-3xl font-bold underline">
-    Hello world!
-  </h1>
-  @include('includes/footer')
-  <script src="https://unpkg.com/flowbite@1.4.1/dist/flowbite.js"></script>
-  <script src="../path/to/flowbite/dist/flowbite.js"></script>
-  @livewireScripts
-</body>
+        <!-- Fonts -->
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
+
+        <!-- Styles -->
+        <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+
+        @livewireStyles
+
+        <!-- Scripts -->
+        <script src="{{ mix('js/app.js') }}" defer></script>
+    </head>
+    <body class="font-sans antialiased">
+        <x-jet-banner />
+
+        <div class="min-h-screen bg-gray-100">
+            @livewire('navigation-menu')
+
+            <!-- Page Heading -->
+            @if (isset($header))
+                <header class="bg-white shadow">
+                    <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+                        {{ $header }}
+                    </div>
+                </header>
+            @endif
+
+            <!-- Page Content -->
+            <main>
+                {{ $slot }}
+            </main>
+        </div>
+
+        @stack('modals')
+
+        @livewireScripts
+    </body>
 </html>
