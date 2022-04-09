927f82fa6234f926bbaa0b495fe7ff0351de1fd2
Has1github <hassanhadimowla3@gmail.com>
2022-04-09 15:04:18 +0300

    footer

--- hassan1/resources/views/layouts/app.blade.php
+++ hassan1/resources/views/layouts/app.blade.php
@@ -17,11 +17,17 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+  <link rel="stylesheet" href="https://unpkg.com/flowbite@1.4.1/dist/flowbite.min.css" />
+  @livewireStyles
 </head>
 <body>
     {{$slot}}
   <h1 class="text-3xl font-bold underline">
     Hello world!
   </h1>
+  @include('includes/footer')
+  <script src="https://unpkg.com/flowbite@1.4.1/dist/flowbite.js"></script>
+  <script src="../path/to/flowbite/dist/flowbite.js"></script>
+  @livewireScripts
 </body>
 </html>
