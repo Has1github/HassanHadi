e14265545e71124dcab4bdc172933816bc835536
Has1github <hassanhadimowla3@gmail.com>
2022-04-09 06:42:24 +0300

    14

--- hassan1/app/Http/Livewire/Test.php
+++ hassan1/app/Http/Livewire/Test.php
@@ -6,6 +6,7 @@ use Livewire\Component;
 
 class Test extends Component
 {
+    
     public function render()
     {
         return view('livewire.test');
--- hassan1/resources/views/layouts/app.blade.php
+++ hassan1/resources/views/layouts/app.blade.php
@@ -1,13 +1,27 @@
-<html>
+{{-- <html>
 <head>
 
     @livewireStyles
 </head>
 <body>
-    <livewire:counter /> 
+    {{$slot}}
  
 
  
     @livewireScripts
+</body>
+</html> --}}
+<!doctype html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+</head>
+<body>
+    {{$slot}}
+  <h1 class="text-3xl font-bold underline">
+    Hello world!
+  </h1>
 </body>
 </html>
